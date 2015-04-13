c6aae945936b044b9d896dab40d27f302085f545
Zach Royer <zroyer@gmail.com>
2015-04-13 18:22:02 -0500

    quest scroll view

--- client/components/questLog/questScrollView.js
+++ client/components/questLog/questScrollView.js
@@ -16,10 +16,34 @@ QuestScroll = {
     var attr = {
       main: {
         class: ctrl.css.main
+      },
+      questsList: {
+        class: ctrl.css.questsList
+      },
+      quest: {
+        class: ctrl.css.quest
+      },
+      bold: {
+        class: ctrl.css.bold
       }
     }
-    return m('div.questScroll', attr.main, 'quests n shit')
 
+
+    return m('div.questsList', attr.questsList, [
+       Quests.find().fetch().map(function (quest) {
+         return m('div.quest', attr.quest, [
+           m('br'),
+           m('br'),
+           m('span', attr.bold, quest.name),
+           m('br'),
+           m('span', 'created by '),
+           m('span', quest.creator),
+           m('br'),
+           m('span','Prize: '),
+           m('span', quest.prize),
+         ])
+       })
+     ])
   },
 
   styles: {
@@ -31,6 +55,27 @@ QuestScroll = {
       'outline': '1px solid red',
       'text-align': 'center',
       'font-size': '2em'
+    },
+    questsList: {
+      'width': '100%',
+      'height': '80%',
+      'padding': '0',
+      'margin': '0',
+      'outline': '1px solid orange',
+      'text-align': 'center',
+      'font-size': '1em'
+    },
+    quest: {
+      'width': '100%',
+      'height': '20%',
+      'padding': '0',
+      'margin': '0',
+      'outline': '1px solid blue',
+      'text-align': 'center',
+      'font-size': '1em'
+    },
+    bold: {
+      'font-weight': 'bold'
     }
   },
 
