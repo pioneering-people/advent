d462f4967ff81bd9c52de9d46bb4f1b00c86d6e8
Zach Royer <zroyer@gmail.com>
2015-04-18 17:16:28 -0500

    added form-control to login

--- client/components/login.js
+++ client/components/login.js
@@ -76,8 +76,8 @@ Login = {
     return m('div', attr.container, [
       m('h3', attr.header, 'Login'), 
       m('form.loginForm', attr.loginForm, [
-        m("input[placeholder='Username'][type='text']", attr.username),
-        m("input[placeholder='Password'][type='text']", attr.password),
+        m("input.form-control[placeholder='Username'][type='text']", attr.username),
+        m("input.form-control[placeholder='Password'][type='text']", attr.password),
         m('button.btn btn-default btn-lg', attr.loginBtn, 'Submit')
       ])
     ])
